001de34c72b5aebd41bd5211c7d2d8fd9567c018
ahmiti <ahmitihassen@gmail.com>
2023-12-27 15:16:13 +0300

    added self signed certificate and created https server that allowsall get requests

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const https = require("https");
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    methods: ["GET"],
+  })
+);
+
+///Create and start https server
+const keyPath = path.join(__dirname, "./cert/key.pem");
+const certPath = path.join(__dirname, "./cert/cert.pem");
+const port = process.env.PORT;
+const options = {};
+
+if (fs.existsSync(keyPath)) {
+  options.key = fs.readFileSync(keyPath);
+}
+
+if (fs.existsSync(certPath)) {
+  options.cert = fs.readFileSync(certPath);
+}
+
+const sslServer = https.createServer(options, app);
+
+sslServer.listen(port, () => {
+  console.log(`Secure server is listening on ${port}`);
+});
