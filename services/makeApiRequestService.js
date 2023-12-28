05533f9433be0cc515d493dcb0281b64b0ae8342
ahmiti <ahmitihassen@gmail.com>
2023-12-28 10:43:14 +0300

    add a function that calls du updater function

--- services/makeApiRequestService.js
+++ services/makeApiRequestService.js
@@ -15,6 +15,12 @@ const makeApiRequest = async () => {
     } catch (err) {
         eapts_sync_logger.error("Error updating items:", err);
     }
+    try {
+        const response = await instance.post("https://localhost:1338/dispensing-unit",{timeout:5000});
+        eapts_sync_logger.info("Dispensing units updated successfully",response);
+    } catch (err) {
+        eapts_sync_logger.error("Error updating dispensing units:", err);
+    }
 }
 
 makeApiRequest();
