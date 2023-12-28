d3c949ef4d5ac1e7d6cd22eb8157ce038e15aedf
ahmiti <ahmitihassen@gmail.com>
2023-12-28 10:52:28 +0300

    format response from eapts

--- services/externalApiService.js
+++ services/externalApiService.js
@@ -1,6 +1,27 @@
 const eapts_sync_logger = require("../utils/eapts_sync_logger");
 const axios = require("axios");
 
+const formatDuResponse = (response) => {
+  for (du in response) {
+    const formattedDu = {
+      institutionName: response[du].institutionName,
+      rowGuid: response[du].rowguid,
+    };
+    return formattedDu;
+  }
+};
+
+const formatItemResponse = (response) => {
+  for (item in response) {
+    const formattedItem = {
+      itemUnit: response[item].itemUnit,
+      institution: response[item].institution,
+      hasStock: response[item].hasStock,
+      quantity: response[item].quantity,
+    };
+    return formattedItem;
+  }
+};
 ///get dispensing unit from eapts
 const getDispensingUnit = async () => {
   const duURL = process.env.DU_URL;
@@ -14,7 +35,7 @@ const getDispensingUnit = async () => {
   try {
     const du = await axios.get(duURL, options);
     eapts_sync_logger.info(du);
-    return du["model"];
+    return formatDuResponse(du?.data?.model);
   } catch (err) {
     eapts_sync_logger.error(err);
     throw err;
@@ -37,7 +58,7 @@ const getItems = async () => {
       try {
         const items = await axios.get(itemStockUrl, options);
         eapts_sync_logger.info(items);
-        return items["model"];
+        return formatItemResponse(items.data.model);
       } catch (err) {
         eapts_sync_logger.error(err);
         throw err;
